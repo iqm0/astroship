f81126988c1ff6eb33169fea841c8d7c7d2fa807
Igor Moreira <igorqm@gmail.com>
2024-06-15 16:20:47 +0100

    design changes for colours and layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,16 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#232424',
+        accent: '#272727',
+      },
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        'text-shadow': '0 1px 1px rgba(0, 0, 0, 0.25)',
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
