0dff39f51f3db73803f4711a86e78b61f4ea0456
Igor Moreira <igorqm@gmail.com>
2024-06-16 13:06:54 +0100

    updatings sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://webfairer.uk",
   integrations: [tailwind(), mdx(), sitemap()],
   devToolbar: {
     enabled: true
